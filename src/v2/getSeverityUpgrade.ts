969df30f5e8208cf261f78784c8881565975efa4
sam <sam@freighttrust.com>
2020-08-30 03:44:18 -0700

    feat: v2+alpha
    new version 2 interface includes a better detailed schema and planned db

---
+++ src/v2/getSeverityUpgrade.ts
@@ -0,0 +1,36 @@
+/**
+ * Enum describing types of severity differences
+ */
+import { Severity } from './types';
+
+export enum SeverityUpgrade {
+  NONE,
+  PATCH,
+  MINOR,
+  MAJOR,
+}
+
+/**
+ * Return the upgrade type from the base severity to the update severity.
+ * Note that downgrades and equivalent severitys are both treated as `NONE`.
+ * @param base base list
+ * @param update update to the list
+ */
+export function getSeverityUpgrade(
+  base: Severity,
+  update: Severity
+): SeverityUpgrade {
+  if (update.major > base.major) {
+    return SeverityUpgrade.MAJOR;
+  }
+  if (update.major < base.major) {
+    return SeverityUpgrade.NONE;
+  }
+  if (update.minor > base.minor) {
+    return SeverityUpgrade.MINOR;
+  }
+  if (update.minor < base.minor) {
+    return SeverityUpgrade.NONE;
+  }
+  return update.patch > base.patch ? SeverityUpgrade.PATCH : SeverityUpgrade.NONE;
+}
